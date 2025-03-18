3eaf5ac497356e18d37c75ed8d62878c228ee74f
StevenFardoux <stevenfardoux@hotmail.fr>
2025-03-18 22:48:00 +0100

    [Feat] recoding a spit function

--- src/Function.cpp
+++ src/Function.cpp
@@ -1,10 +1,18 @@
 #include "Fuction.hpp"
 #include <vector>
 
-String Split(String str, char delimiter) 
+std::vector<String> Split(String str, char delimiter) 
 {
+    std::vector<String> tabRes;
     String res = "";
-    int pos = str.indexOf(delimiter);
+    int pos;
+    
+    while ((pos = str.indexOf(delimiter)) != -1)
+    {
+        tabRes.push_back(str.substring(0, pos));
+        str.remove(0, pos + 1);
+    }
+    tabRes.push_back(str);
 
-
+    return tabRes;
 }
